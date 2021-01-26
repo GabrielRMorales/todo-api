f71ba459f9d910d1d131bd5bf5773f4b5c1f54d2
Gabriel Morales <grmorale@gmail.com>
2021-01-25 20:48:36 -0800

    Set up basic REST API functionality

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 const port = 3000;
 const { Pool } = require("pg");
+const bodyParser = require("body-parser");
+const { response } = require("express");
 
 const pool = new Pool({
     user: "gabriel",
@@ -10,6 +12,13 @@ const pool = new Pool({
     password: null,
     port: 5432
 });
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+//index
 app.get("/",(req,res)=>{
     console.log("getting todo list");
     pool.query("SELECT * FROM todo_list",(err, results)=>{
@@ -21,7 +30,44 @@ app.get("/",(req,res)=>{
     });
 });
 
+//create
+app.post("/", (req, res)=>{
+    const { name } = req.body;
+    console.log("Got name")
+    pool.query(`INSERT INTO todo_list(name)VALUES($1)`,[name], (err, results)=>{
+        if (err){
+            throw err;
+        }
+        console.log("INSERTED");
+        res.status(201).send(`Todo added with name ${name}`);
+    });
 
+})
+
+//update
+app.put("/:id",(req,res)=>{
+    const id = req.params.id;
+    
+    pool.query(`UPDATE todo_list SET completed = NOT completed WHERE id = $1 RETURNING completed`,[id],(err,newStatus)=>{
+        if (err){
+            throw err
+        }
+        console.log(newStatus.rows[0].completed);
+        res.status(200).send(`Todo completed status has been updated to ${newStatus.rows[0].completed}`);
+    });
+
+});
+
+//delete
+app.delete("/:id",(req, res)=>{
+    const id = req.params.id;
+    pool.query(`DELETE FROM todo_List WHERE id=$1`,[id],(err, results)=>{
+        if (err){
+            throw err;
+        }
+        res.status(200).send(`Deleted item of id ${id}`);
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`App is now running on port ${port}`);
