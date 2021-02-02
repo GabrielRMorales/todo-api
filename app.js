18fae518370e51e5ba1cf6defcd11feabd07a695
Gabriel Morales <grmorale@gmail.com>
2021-02-01 19:48:07 -0800

    Improve UI

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 const port = 3000;
-//get Pool out of it
+//get Pool 
 const  Pool  = require("pg").Pool;
 const bodyParser = require("body-parser");
 const { response, static } = require("express");
@@ -16,7 +16,7 @@ const pool = new Pool({
 });
 app.use(express.static(__dirname+"/public"));
 app.use("/scripts", express.static(__dirname+"/node_modules/jquery/dist/"));
-//These middlewares are necessary to retrieve req.body
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
@@ -28,26 +28,21 @@ app.get("/",(req,res)=>{
 
 //index
 app.get("/api/todos/",(req,res)=>{
-    console.log("getting todo list");
+    
     pool.query("SELECT * FROM todo_list ORDER BY id ASC",(err, results)=>{
         if (err){
             throw err;
         }
-        console.log("SENDING RESULTS...");
         res.send(results.rows);
     });
 });
 
-//create-NOTE YOU NEED THE RETURNING part to get back any data of the newly created one
-//ALSO, this should not take params, it should get the data in the body, included in the post as an object
 app.post("/api/todos/", (req, res)=>{
     const  {name}  = req.body;
-    console.log(req.body);
     pool.query(`INSERT INTO todo_list(name) VALUES ($1) RETURNING *`,[name], (err, results)=>{
         if (err){
             throw err;
         }
-       // console.log(results.rows[0]);
         res.status(201).send(results);
     });
 
--- public/main.js
+++ public/main.js
@@ -1,77 +1,78 @@
 $(document).ready(function () {
     const localURL = "http://localhost:3000/api/todos/";
 
-    const deleteTodo=(parent)=>{
-        let elId=parent.data().id;
-        parent.remove();        
-        $.ajax({type: "DELETE",
-        url: localURL+elId});       
-        
+    const deleteTodo = (parent) => {
+        let elId = parent.data().id;
+        parent.remove();
+        $.ajax({
+            type: "DELETE",
+            url: localURL + elId
+        });
+
     }
-    const addDelete=()=>{
+    const addDelete = () => {
         let btn = $("<span class='delete-button'> X</span>");
-        btn.click(function(e){
-         e.stopPropagation();
-         deleteTodo($(e.target).closest("li"));
-          });
+        btn.click(function (e) {
+            e.stopPropagation();
+            deleteTodo($(e.target).closest("li"));
+        });
         return btn;
     }
 
-    const addTodo=(todo)=>{
-       
-       /* let inputValue = $("#new-todo").val();
-        $.post({url: localURL+inputValue });
-        $("#todo-list").append(`<li>${inputValue}<span class="delete"> X</span></li>`)
-        $(".delete").click(function(e){
-            e.stopPropagation();
-            console.log($(this).parent())
-            deleteTodo()
-        });
-       */
-        let $listItem=$("<li></li>");
-        if (todo.completed){
+    const addTodo = (todo) => {
+
+        let $listItem = $("<li></li>");
+        if (todo.completed) {
             $listItem.addClass("completed");
         }
-        $listItem.data("id",todo.id);
-        //make deleter
+        $listItem.data("id", todo.id);
         let $deleteButton = addDelete();
-        //
-       
-        $listItem.html(`${todo.name}`).append($deleteButton);
-        $listItem.click(function(){
+
+        $listItem.html(`<span class="todo-description">${todo.name}</span>`).append($deleteButton);
+        $listItem.click(function() {
             $(this).toggleClass("completed");
-            $.ajax({type: "PUT", url: localURL+$listItem.data().id});
+            $.ajax({
+                type: "PUT",
+                url: localURL + $listItem.data().id
+            });
         });
         $("#todo-list").append($listItem);
 
     }
 
-    const getTodos = ()=>{
+    const getTodos = () => {
         $.get({
-            url: localURL
-        })
-        .then(data =>{
-          
-           data.forEach(todo=>{
-               addTodo(todo);               
-            });
-
-        })
-        .fail(err=>console.log(err));
+                url: localURL
+            })
+            .then(data => {
+                data.forEach(todo => {
+                    addTodo(todo);
+                });
+            })
+            .fail(err => console.log(err));
     }
-    
+
     getTodos();
-    
-    $("#add-todo").click(function(e){
+
+    $("#add-todo").click(function (e) {
         e.preventDefault();
         let inputValue = $("#new-todo").val();
-        //note that you need to add the data here for a body and the datatype for it to be properly read
-        $.post({url: localURL, data: { "name": inputValue }, dataType: "json" }).then(data=>{
-            addTodo(data.rows[0]);
-        });
-       
+        if (inputValue.trim()!==""){
+            $.post({
+                url: localURL,
+                data: {
+                    "name": inputValue
+                },
+                dataType: "json"
+            }).then(data => {
+                addTodo(data.rows[0]);
+            });
+        } else {
+            alert("Can't accept blank value.");
+        }
+        
+
         $("#new-todo").val("");
     });
 
-    
 });
